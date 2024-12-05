e5524767e3e79c4f7b078d186ffc0aa4a265034b
VitorSilva <vitorsilvaholleweger@gmail.com>
2024-12-05 10:12:47 -0300

    addind classes generated from starUML

---
+++ code/game.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.window Surface = None
+
+    def window(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
+    def run(self, ):
+        pass
---
+++ code/menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.window = None
+
+    def window(self, ):
+        pass
+
+    def run(self, ):
+        pass
