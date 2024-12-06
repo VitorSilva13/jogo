39fc1a9c1457f8cbc5782f6452ef983da75fd55d
VitorSilva <vitorsilvaholleweger@gmail.com>
2024-12-06 12:07:03 -0300

    sim

--- code/menu.py
+++ code/menu.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import WIN_WIDTH
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
-
-    def window(self, ):
-        pass
+        self.surf = pygame.image.load('./assets/menubg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer.music.load('./assets/musica game.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=50, text="Zombie Killer", text_color=(28, 14, 64), text_center_pos=((WIN_WIDTH/ 2),70))
+            pygame.display.flip()
+
+            # Check all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # End pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, antialias=True).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
