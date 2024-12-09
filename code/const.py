de59ac7360ead7df49c25c1f3c2c8769e7e7280e
VitorSilva <vitorsilvaholleweger@gmail.com>
2024-12-09 09:26:55 -0300

    menu

--- code/const.py
+++ code/const.py
@@ -1,2 +1,13 @@
+# C
+COLOR_PURPLE = (28, 14, 64)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME',
+               'OPTIONS' ,
+               'SCORE' ,
+               'EXIT')
+
+# W
 WIN_WIDTH = 500
 WINDOW_HEIGHT = 400
--- code/menu.py
+++ code/menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH
+from code.const import WIN_WIDTH, COLOR_PURPLE, MENU_OPTION, COLOR_WHITE
 
 
 class Menu:
@@ -18,7 +18,14 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(text_size=50, text="Zombie Killer", text_color=(28, 14, 64), text_center_pos=((WIN_WIDTH/ 2),70))
+            self.menu_text(text_size=50, text="Zombie Killer", text_color=COLOR_PURPLE,
+                           text_center_pos=((WIN_WIDTH / 2), 70))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(text_size=30, text=MENU_OPTION[i], text_color=COLOR_WHITE,
+                               text_center_pos=((WIN_WIDTH / 2), 200 + 30 * i))
+
+
             pygame.display.flip()
 
             # Check all events
@@ -29,6 +36,6 @@ class Menu:
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, antialias=True).convert_alpha()
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
